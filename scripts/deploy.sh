36578441d1f115b655c24d7e20afa76c62a77658
DJ <deepakj6@yahoo.co.in>
2021-08-01 05:10:29 +0100

    Pipeline 1.3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,6 +3,14 @@ set -e
 
 echo Namespace = "$2"
 echo Releasename = "$1"
+
+
+echo "-----------------------"
+echo 0="$0"
+echo 1="$1"
+echo 2="$2"
+echo 3="$3"
+
 echo "----------------------Installing pages----------------------------------"
 
 
